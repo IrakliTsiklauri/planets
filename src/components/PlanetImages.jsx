ead2564dc6da0e03ee3aba382f485739b76ccea3
IrakliTsiklauri <125734135+IrakliTsiklauri@users.noreply.github.com>
2024-04-28 16:33:35 +0400

    add mobile size for geology images

--- src/components/PlanetImages.jsx
+++ src/components/PlanetImages.jsx
@@ -17,6 +17,8 @@ const PlanetImages = ({
   const tabletGeoHeight = planetData.tabGeologyImgSize.tabGeoImgHeight;
   const mobileImgWidth = planetData.mobileSize.mobWidth;
   const mobileImgHeight = planetData.mobileSize.mobHeight;
+  const mobileGeoImgWidth = planetData.mobileGeologiImgSize.mobGeoImgWidth;
+  const mobileGeoImgHeight = planetData.mobileGeologiImgSize.mobGeoImgHeight;
 
   return (
     <ImgSection>
@@ -29,6 +31,8 @@ const PlanetImages = ({
           imgheight={imgheight}
           tabletimgwidth={tabletimgwidth}
           tabletimgheight={tabletimgheight}
+          mobileImgWidth={mobileImgWidth}
+          mobileImgHeight={mobileImgHeight}
         />
       )}
       {activeBtn === "geology" && (
@@ -41,6 +45,8 @@ const PlanetImages = ({
             geologyimgheight={geologyimgheight}
             tabletGeoWidth={tabletGeoWidth}
             tabletGeoHeight={tabletGeoHeight}
+            mobileGeoImgWidth={mobileGeoImgWidth}
+            mobileGeoImgHeight={mobileGeoImgHeight}
           />
 
           <Image
@@ -51,6 +57,8 @@ const PlanetImages = ({
             imgheight={imgheight}
             tabletimgwidth={tabletimgwidth}
             tabletimgheight={tabletimgheight}
+            mobileImgWidth={mobileImgWidth}
+            mobileImgHeight={mobileImgHeight}
           />
         </>
       )}
@@ -83,23 +91,26 @@ const Image = styled.img`
   @media (max-width: 768px) {
     width: ${(props) => props.tabletimgwidth};
     height: ${(props) => props.tabletimgheight};
-
-    @media (max-width: 400px) {
-      width: 111px;
-      height: 111px;
-    }
+  }
+  @media (max-width: 400px) {
+    width: ${(props) => props.mobileImgWidth};
+    height: ${(props) => props.mobileImgHeight};
   }
 `;
 
 const Img = styled.img`
   position: absolute;
   transform: translate(0, 90%);
-  /* top: 83%; */
   width: ${(props) => props.geologyimgwidth};
   height: ${(props) => props.geologyimgheight};
 
   @media (max-width: 768px) {
     width: ${(props) => props.tabletGeoWidth};
     height: ${(props) => props.tabletGeoHeight};
+  }
+
+  @media (max-width: 400px) {
+    width: ${(props) => props.mobileGeoImgWidth};
+    height: ${(props) => props.mobileGeoImgHeight};
   }
 `;
